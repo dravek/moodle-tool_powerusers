7fe1b87db84183e61eb1fc51e817f76de61e35f7
David Carrillo <davidmc@moodle.com>
2026-03-02 17:23:42 +0100

    Release version 1.1.2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tool_powerusers';
-$plugin->version      = 2026022700;
+$plugin->version      = 2026030201;
 $plugin->requires     = 2015111600;
-$plugin->release      = '1.1.0';
+$plugin->release      = '1.1.2';
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [];
